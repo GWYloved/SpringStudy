78f8128a3511e4dfde6f9e5261606bd78c2f7c1d
whiteGive <913925047@qq.com>
2019-04-03 19:23:29 +0800

    update

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,15 +1,68 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class HelloController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(){
-        return "hello world!";
+    public HelloJson hello() {
+        return new HelloJson("world");
     }
 
+    @RequestMapping(value = "/file", method = RequestMethod.GET)
+    public ResponseEntity<FileSystemResource> file() {
+        return export(new File("D:\\jubang.sql"));
+    }
 
+    /**
+     * 返回文件
+     * @param file
+     * @return
+     */
+    public ResponseEntity<FileSystemResource> export(File file) {
+        if (file == null) {
+            return null;
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Content-Disposition", "attachment; filename=" + System.currentTimeMillis() + ".mp4");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        headers.add("Last-Modified", new Date().toString());
+        headers.add("ETag", String.valueOf(System.currentTimeMillis()));
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(new FileSystemResource(file));
+    }
+
+    public class HelloJson {
+        List<String> name = new ArrayList<>();
+
+        public HelloJson(String name) {
+            this.name.add(name);
+            this.name.add(name);
+        }
+
+        public List<String> getName() {
+            return name;
+        }
+
+        public void setName(List<String> name) {
+            this.name = name;
+        }
+    }
 }
