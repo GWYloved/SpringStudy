02909bc0bbce92a9d88fb11f44014010a50fe891
whiteGive <913925047@qq.com>
2019-04-04 16:34:56 +0800

    update

---
+++ src/main/java/com/example/demo/PostController.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+//    /**
+//     * 普通的post
+//     * @return
+//     */
+//    @RequestMapping(value = "/post/hello", method = RequestMethod.POST)
+//    public String postHello(){
+//        return "hello";
+//    }
+//
+//    /**
+//     * 带参数的post
+//     * @param name
+//     * @return
+//     */
+//    @RequestMapping(value = "/post/hello", method = RequestMethod.POST)
+//    public String postHello(@RequestParam String name){
+//        return "hello"+name;
+//    }
+
+//    @RequestMapping(value = "/post/hello",method = RequestMethod.POST)
+
+}
